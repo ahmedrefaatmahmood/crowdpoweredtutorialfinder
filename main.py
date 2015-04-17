6b0e94085fb7279bbbb1f3eeb6ed67aa994220c3
Nelson0409 <dmengdi@gmail.com>
2015-04-17 09:40:36 -0400

    this is a test change

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ no_count=0
 tutorials=['t1','t2','t3','t4','t5','t6','t7','t8','t9','t10']
 
 def init_db():
-    """Creates the database tables."""
+    """Creates the database tables.test change"""
 	
     with app.app_context():
 		print "creating database"
